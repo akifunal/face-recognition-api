047abf3a727344a4e5ba0c50f7970094a6ab2bcf
akif <akifunal05@gmail.com>
2023-03-07 20:58:59 +0300

    chore: route descriptions added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,18 +15,26 @@ import {
  * @param {Object} options plugin options, refer to https://www.fastify.io/docs/latest/Reference/Plugins/#plugin-options
  */
 async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
-	fastify.get('/', get)
 
+	// get all users
+fastify.get('/', get)
+
+	// ping
 	fastify.get('/ping', getPing)
 
+	// sign in
 	fastify.post('/signin', postSignIn)
 
+	// register
 	fastify.post('/register', postRegister)
 
+	// get all users
 	fastify.get('/users', getUsers)
 
+	// get profile by id
 	fastify.get('/profile/:id', getProfileById)
 
+	// update image
 	fastify.put('/image', putImage)
 }
 
